d6ef0ef14aeb37affdb3d604052b3162df189940
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-14 08:02:25 -0300

    Criado o componente App

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+// modules => alias: __dirname + '/node_modules' | Definido em webpack.config.js
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+export default props => (
+    <div className="container">
+        <h1>Teste</h1>
+    </div>
+)
