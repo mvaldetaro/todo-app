c1d4fcbdac9a017f369b8b37e762d395e4d01abf
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-15 08:05:43 -0300

    Adicionado metodos par o onChange do formulário

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,32 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        // metodo obrigatório do construtor
+        super(props)
+        // definindo o estado inicial da aplicação
+        this.state = {description: '', list: [] }
+        // definindo o contexto do this
+        this.handleAdd = this.handleAdd.bind(this) 
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({description: e.target.value })
+    }
+
+    handleAdd(){
+        console.log(this.state.description)
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm/>
+                <TodoForm 
+                    description={this.state.description} 
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}/>
                 <TodoList/>
             </div>
         )
