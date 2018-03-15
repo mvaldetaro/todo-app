2d47d998d92fcd5453102ed4f1254bb85fdb4965
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-14 22:42:06 -0300

    Revert "Atualiza o componete todoForm"
    This reverts commit 639d400d282d2086236720c8efe2b3bf7af2f726.

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
 
 export default props => (
-    <div role='form' className='todoForm'>
-        <div className='col-xs-12 col-sm-9 col-md-10'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>
-        </div>
-
-        <div className='col-xs12 col-sm-3 col-md-2'>
-            <button className='btn btn-primary'>
-                <i className='fa fa-plus'></i>
-            </button>
-        </div>
+    <div>
+        <h1>Form</h1>
     </div>
 )
