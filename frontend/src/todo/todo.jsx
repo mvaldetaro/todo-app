77ffe3ae430ceacc637b464adbfe7e3a1248b46a
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-15 23:46:26 -0300

    Consulta e Exclusão de tarefas

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -16,6 +16,14 @@ export default class Todo extends Component {
         // definindo o contexto do this
         this.handleAdd = this.handleAdd.bind(this) 
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=createdAt`)
+            .then(resp => this.setState({description:'', list: resp.data}))
     }
 
     handleChange(e) {
@@ -25,7 +33,12 @@ export default class Todo extends Component {
     handleAdd(){
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('Funcionou'))
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
 
     render() {
@@ -36,7 +49,7 @@ export default class Todo extends Component {
                     description={this.state.description} 
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd}/>
-                <TodoList/>
+                <TodoList list={this.state.list} handleRemove={this.handleRemove}/>
             </div>
         )
     }
