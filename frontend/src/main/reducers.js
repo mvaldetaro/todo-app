211cee0633b53e4da20be75e4f472271c2e71dca
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-17 19:56:36 -0300

    Reducer estático

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,26 @@
+import { combineReducers } from "redux";
+
+const rootReducer = comebineReducers({
+    todo: () => ({
+        description: 'Ler Livros',
+        list: [
+            {
+                _id: 1,
+                description: 'Pagar fatura do cartão',
+                done: true
+            },
+            {
+                _id: 2,
+                description: 'Reunião com a equipe',
+                done: false
+            },
+            {
+                _id: 3,
+                description: 'Teste de React',
+                done: false
+            },
+        ]
+    }) 
+})
+
+export default rootReducer
