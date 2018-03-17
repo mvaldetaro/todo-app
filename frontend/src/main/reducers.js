bd26c6293df210c05fa441e5a76ee3dd657668d5
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-17 20:11:34 -0300

    Integrando o Reducers

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-const rootReducer = comebineReducers({
+const rootReducer = combineReducers({
     todo: () => ({
         description: 'Ler Livros',
         list: [
