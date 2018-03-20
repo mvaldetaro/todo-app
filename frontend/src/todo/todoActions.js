a257801e4f979ece984e7ed34718b2965e1960aa
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-19 23:00:26 -0300

    ActionCreator da pesquisa

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -1,4 +1,17 @@
+import axios from 'axios'
+
+const URL = 'http://localhost:4004/api/todos'
+
 export const changeDescription = event => ({
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 })
+
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
+
